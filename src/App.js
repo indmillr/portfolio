4060e51651e874ce47bd37f7c72fca4950184972
Ian Miller <theiandmiller@gmail.com>
2022-03-26 07:12:22 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavMenu from "./components/NavMenu";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <div>
-      <h1>Welcome to React Portfolio!</h1>
-      <h3>- Ian</h3>
+      <Router>
+        <NavMenu />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+      </Router>
     </div>
   );
 }
