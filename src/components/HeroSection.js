4a53cdb1345aa73259f51f4ada4fc9fa2efe0fe6
Ian Miller <theiandmiller@gmail.com>
2022-03-28 05:37:48 -0500

    Button Component

---
+++ src/components/HeroSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HeroImg from "../assets/images/hero.png";
+import Button from "./Button";
+import PText from "./PText";
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div className='container'>
+        <h1 className='hero__heading'>
+          <span>Hello this is</span>
+          <span>Ian Miller</span>
+        </h1>
+        <div className='hero__img'>
+          <img src={HeroImg} alt='' />
+        </div>
+        <div className='hero__info'>
+          <PText>
+            I love to design and make new web experiences for people.
+          </PText>
+          <Button btnLink='/projects' btnText='See my Works' outline={false} />
+        </div>
+      </div>
+    </div>
+  );
+}
