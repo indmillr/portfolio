80358ea589a76e3e74d5fd891b8d04ac724e1651
Ian Miller <theiandmiller@gmail.com>
2022-03-28 05:54:15 -0500

    Social and Scroll Arrows

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,8 @@ import React from "react";
 import HeroImg from "../assets/images/hero.png";
 import Button from "./Button";
 import PText from "./PText";
+import SocialMediaArrow from "../assets/images/social-media-arrow.svg";
+import ScrollDownArrow from "../assets/images/scroll-down-arrow-.svg";
 
 export default function HeroSection() {
   return (
@@ -19,6 +21,43 @@ export default function HeroSection() {
             I love to design and make new web experiences for people.
           </PText>
           <Button btnLink='/projects' btnText='See my Works' />
+        </div>
+        <div className='hero__social'>
+          <div className='hero__social__indicator'>
+            <p>Follow</p>
+            <img src={SocialMediaArrow} alt='' />
+          </div>
+          <div className='hero_social_text'>
+            <ul>
+              <li>
+                <a
+                  href='https://github.com/indmillr'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  GH
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.linkedin.com/in/indmillr'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  LI
+                </a>
+              </li>
+              <li>
+                <a href='https://facebook.com' target='_blank' rel='noreferrer'>
+                  FB
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='hero__scrollDown'>
+          <p>Scroll</p>
+          <img src={ScrollDownArrow} alt='' />
         </div>
       </div>
     </div>
