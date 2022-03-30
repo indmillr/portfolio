8b3b811c0d4e95bd9bd932f4b54bc74371053c59
Ian Miller <theiandmiller@gmail.com>
2022-03-30 05:34:37 -0500

    Testimonial Section init

---
+++ src/components/TestimonialSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionTitle from "./SectionTitle";
+
+export default function TestimonialSection() {
+  return (
+    <div>
+      <div className='container'>
+        <SectionTitle
+          heading='Testimonials'
+          subheading='See what everyone says'
+        />
+      </div>
+    </div>
+  );
+}
