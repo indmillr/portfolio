656e593baf495a53f5d0f4cba79a969c38df773e
Ian Miller <theiandmiller@gmail.com>
2022-03-31 05:12:44 -0500

    new About section component

--- src/pages/About2.js
+++ src/pages/About2.js
@@ -3,6 +3,8 @@ import Button from "../components/Button";
 import PText from "../components/PText";
 import AboutImg from "../assets/images/about-page-img.png";
 import styled from "styled-components";
+import AboutInfoItem from "../components/AboutInfoItem";
+import ContactBanner from "../components/ContactBanner";
 
 const AboutPageStyles = styled.div`
   padding: 20rem 0 10rem 0;
@@ -42,11 +44,37 @@ const AboutPageStyles = styled.div`
       border: 2px solid var(--gray-1);
     }
   }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-bottom: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
 `;
 
 export default function About2() {
   return (
-    <div>
+    <AboutPageStyles>
       <div className='container'>
         <div className='top-section'>
           <div className='left'>
@@ -82,7 +110,57 @@ export default function About2() {
             <img src={AboutImg} alt='Ian Miller img' />
           </div>
         </div>
+        <div className='about__info__items'>
+          <div className='about__info__item'>
+            <h1 className='about__info__heading'>Education</h1>
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+          </div>
+
+          <div className='about__info__item'>
+            <h1 className='about__info__heading'>Skills</h1>
+            <AboutInfoItem
+              title='FrontEnd'
+              items={["HTML", "CSS", "JS", "REACT"]}
+            />
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+          </div>
+
+          <div className='about__info__item'>
+            <h1 className='about__info__heading'>Experience</h1>
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+            <AboutInfoItem
+              title='School'
+              items={["Missouri State University"]}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+      <ContactBanner />
+    </AboutPageStyles>
   );
 }
