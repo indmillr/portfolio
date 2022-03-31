03f1176789bf5d1f036abb48728135737b0f684f
Ian Miller <theiandmiller@gmail.com>
2022-03-31 06:15:37 -0500

    Contact Form and Contact Info Item components init

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { MdEmail, MdLocalPhone } from "react-icons/md";
 import styled from "styled-components";
+import ContactInfoItem from "./ContactInfoItem";
 import SectionTitle from "./SectionTitle";
 
 const ContactSectionStyle = styled.div`
-  padding: 10rem;
+  padding: 10rem 0;
 `;
 
 export default function ContactSection() {
   return (
-    <div>
+    <ContactSectionStyle>
       <div className='container'>
         <SectionTitle heading='Contact' subheading='Get in touch' />
+        <div className='contactSection__wrapper'>
+          <div className='left'>
+            <ContactInfoItem icon={<MdLocalPhone />} text='(123) 456-7890' />
+            <ContactInfoItem icon={<MdEmail />} text='indmillr@gmail.com' />
+            <ContactInfoItem text='Nixa, MO USA' />
+          </div>
+          <div className='right'>Contact Form</div>
+        </div>
       </div>
-    </div>
+    </ContactSectionStyle>
   );
 }
